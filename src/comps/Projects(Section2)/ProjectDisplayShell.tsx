01aec5e805ccfb132374aae3a2cae9556f4aab8b
Milo Chambers <chambersmilo@gmail.com>
2024-12-17 17:37:49 +0000

    refactor: move demo display state to individual project components. Extract project display logic into the new ProjectItem component. Add local state management for desktop/mobile toggle, with each project now independently handling its display state.

--- src/comps/Projects(Section2)/ProjectDisplayShell.tsx
+++ src/comps/Projects(Section2)/ProjectDisplayShell.tsx
@@ -1,71 +1,26 @@
 import { useState } from "react";
 
-import DeviceShell from "./DeviceShell";
+import ProjectItem from "./ProjectItem";
 import MobileDeviceShell from "./DeviceComps/MobileDeviceShell";
-import DualPillTab from "../UtilityComps/DualPillTab";
 import ProjectInfoShell from "./ProjectInfoShell";
 
 import { GAProjects } from "../../data/TextContent/projectsText/projects";
-import { IProject } from "../../data/TextContent/projectsText/projects";
 
 const gaProjectsArray = GAProjects;
 
 const ProjectDisplayShell = () => {
-  const [demoDisplay, setDemoDisplay] = useState("desktop");
-
-  const formatProjects = (project: IProject, projectNum: number) => {
-    if (projectNum % 2 === 0) {
-      return (
-        <>
-          <div className="col-span-1">
-            <div className="flex justify-center">
-              <DualPillTab
-                demoDisplay={demoDisplay}
-                setDemoDisplay={setDemoDisplay}
-                option1={"desktop"}
-                option2={"mobile"}
-              />
-            </div>
-            <ProjectInfoShell project={project} />
-          </div>
-          <div className="col-span-1">
-            <DeviceShell projectDemo={project.demo} demoDisplay={demoDisplay} />
-          </div>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <div className="col-span-1">
-            <DeviceShell projectDemo={project.demo} demoDisplay={demoDisplay} />
-          </div>
-          <div className="col-span-1">
-            <div className="flex justify-center">
-              <DualPillTab
-                demoDisplay={demoDisplay}
-                setDemoDisplay={setDemoDisplay}
-                option1={"desktop"}
-                option2={"mobile"}
-              />
-            </div>
-            <ProjectInfoShell project={project} />
-          </div>
-        </>
-      );
-    }
-  };
-
   return (
     <section id="my-projects" className="pt-[60px]">
       <div id="ga-projects-desktop-view" className="hidden md:block space-y-8">
         {gaProjectsArray.map((project) => (
           <article
             key={project.projectNum}
-            id={`ga-project-desktop-${project.projectNum}`}
+            id={`ga-project-desktop-display-${project.projectNum}`}
           >
-            <div className="grid grid-cols-2 gap-4">
-              {formatProjects(project, project.projectNum)}
-            </div>
+            <ProjectItem
+              project={project}
+              isEven={project.projectNum % 2 === 0}
+            />
           </article>
         ))}
       </div>
---
+++ src/comps/Projects(Section2)/ProjectItem.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import DualPillTab from "../UtilityComps/DualPillTab";
+import DeviceShell from "./DeviceShell";
+import ProjectInfoShell from "./ProjectInfoShell";
+import { IProject } from "../../data/TextContent/projectsText/projects";
+
+interface ProjectItemProps {
+  project: IProject;
+  isEven: boolean;
+}
+
+const ProjectItem = ({ project, isEven }: ProjectItemProps) => {
+  const [demoDisplay, setDemoDisplay] = useState("desktop");
+
+  if (isEven) {
+    return (
+      <div className="grid grid-cols-2 gap-4">
+        <div className="col-span-1">
+          <div className="flex justify-center">
+            <DualPillTab
+              demoDisplay={demoDisplay}
+              setDemoDisplay={setDemoDisplay}
+              option1={"desktop"}
+              option2={"mobile"}
+            />
+          </div>
+          <ProjectInfoShell project={project} />
+        </div>
+        <div className="col-span-1">
+          <DeviceShell projectDemo={project.demo} demoDisplay={demoDisplay} />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <div className="col-span-1">
+        <DeviceShell projectDemo={project.demo} demoDisplay={demoDisplay} />
+      </div>
+      <div className="col-span-1">
+        <div className="flex justify-center">
+          <DualPillTab
+            demoDisplay={demoDisplay}
+            setDemoDisplay={setDemoDisplay}
+            option1={"desktop"}
+            option2={"mobile"}
+          />
+        </div>
+        <ProjectInfoShell project={project} />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
